fa52e78c40e76ddad54f4008b77f852ab4100f2a
Oleksandr Voloshanov <oleksandr.voloshanov@deviark.com>
2026-06-12 11:48:28 +0300

    Added tests target

---
+++ TalkTests/TalkTests.swift
@@ -0,0 +1,18 @@
+//
+//  TalkTests.swift
+//  TalkTests
+//
+//  Created by Oleksandr Voloshanov on 12.06.2026.
+//
+
+import Testing
+
+struct TalkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
